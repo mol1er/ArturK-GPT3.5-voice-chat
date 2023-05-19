45decd452d36b6fadc71f4baef09996cbd539bf3
KENZINAZ-LAPTOP\user_adm <artur@kenzin.ru>
2023-05-19 14:31:08 +0300

    bag fix

--- src/main.js
+++ src/main.js
@@ -1,85 +1,111 @@
-import { Telegraf, session } from 'telegraf'
-import { message } from 'telegraf/filters'
-import config from 'config'
-import { ogg } from './ogg.js'
-import { openai } from './openai.js'
-
+import { Telegraf, session } from 'telegraf';
+import { message } from 'telegraf/filters';
+import config from 'config';
+import { ogg } from './ogg.js';
+import { openai } from './openai.js';
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-const INITIAL_SESSION = {
-    messages: [],
+function createUserSession() {
+    return {
+        session: {
+            messages: [],
+        },
+        context: {},
+    };
 }
 
-const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
+const bot = new Telegraf(config.get('TELEGRAM_TOKEN'));
 
-bot.use(session())
+bot.use(session());
 
 bot.command('new', async (ctx) => {
-    ctx.session = INITIAL_SESSION
-})
+    const { session, context } = createUserSession();
+    ctx.session = session;
+    ctx.context = context;
+
+    // Сообщение о перезапуске бота
+    const restartMessage = 'The bot has been restarted.';
+    await ctx.reply(restartMessage);
+});
 
 bot.command('start', async (ctx) => {
-    ctx.session = INITIAL_SESSION
+    const { session, context } = createUserSession();
+    ctx.session = session;
+    ctx.context = context;
 
-})
+    // Приветственное сообщение
+    const welcomeMessage = `Hello! I'm the first voice GPT chat by @mol1er. Write me your question using a voice message or text.`;
+
+    await ctx.reply(welcomeMessage);
+});
 
 bot.on(message('voice'), async (ctx) => {
-    if (!ctx.session) {
-        INITIAL_SESSION
-    }
+    const { session, context } = createUserSession();
+    ctx.session = session;
+    ctx.context = context;
 
     try {
-        const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
-        const userId = String(ctx.message.from.id)
-        const oggPath = await ogg.create(link.href, userId)
-        const mp3Path = await ogg.toMp3(oggPath, userId)
+        const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+        const userId = String(ctx.message.from.id);
+        const oggPath = await ogg.create(link.href, userId);
+        const mp3Path = await ogg.toMp3(oggPath, userId);
 
-        const text = await openai.transcription(mp3Path)
+        const text = await openai.transcription(mp3Path);
 
         ctx.session.messages.push({
             role: openai.roles.USER,
-            content: text
-        })
+            content: text,
+        });
 
-        const response = await openai.chat(ctx.session.messages)
+        await ctx.reply('Working on your question...'); // Сообщение "Работаю над вашим вопросом"
+
+        const response = await openai.chat(ctx.session.messages, ctx.context);
 
         ctx.session.messages.push({
             role: openai.roles.ASSISTANT,
-            content: response.content
-        })
+            content: response.content,
+        });
 
-        await ctx.reply(response.content)
+        ctx.context = response.context;
+
+        await ctx.reply(response.content);
     } catch (e) {
-        console.log('ERROR while voice message', e.message)
+        console.log('ERROR while voice message', e.message);
+        await ctx.reply('BOT SLOMALSYA...');
     }
-})
+});
 
 bot.on(message('text'), async (ctx) => {
-    if (!ctx.session) {
-        INITIAL_SESSION
-    }
+    const { session, context } = createUserSession();
+    ctx.session = session;
+    ctx.context = context;
 
     try {
         ctx.session.messages.push({
             role: openai.roles.USER,
-            content: ctx.message.text
-        })
+            content: ctx.message.text,
+        });
 
-        const response = await openai.chat(ctx.session.messages)
+        await ctx.reply('Working on your question...'); // Сообщение "Работаю над вашим вопросом"
+
+        const response = await openai.chat(ctx.session.messages, ctx.context);
 
         ctx.session.messages.push({
             role: openai.roles.ASSISTANT,
-            content: response.content
-        })
+            content: response.content,
+        });
 
-        await ctx.reply(response.content)
+        ctx.context = response.context;
+
+        await ctx.reply(response.content);
     } catch (e) {
-        console.log('ERROR while text message', e.message)
+        console.log('ERROR while text message', e.message);
+        await ctx.reply('BOT SLOMALSYA...');
     }
-})
+});
 
-bot.launch()
+bot.launch();
 
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
